193a53b5a96eeb4187e356513653c54625f55609
Yvonneygb <git@github.com:Yvonneygb/xingxingmama.git>
2017-05-12 18:42:52 +0800

    personal_center

---
+++ xingxingmama/application/views/personal_center.php
@@ -0,0 +1,157 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8" >
+    <title>个人中心</title>
+	<link rel="stylesheet" href="http://127.0.0.1/xingxingmama/xingxingmama/resource/css/personal_center.css">
+	<link rel="stylesheet" href="http://127.0.0.1/xingxingmama/xingxingmama/resource/css/style1.css">
+
+	
+</head>
+
+<body>
+	<div class="top">			
+		<div class="top_pule_color">
+		</div>
+		
+		<div class="top_under">
+			<div class="avatar">
+				<img src="D://xampp/htdocs/xingxingmama/xingxingmama/resource/images/da8e974dc_xs.jpg" alt="">
+			</div>
+			
+			<div class="name_and_information_box">
+				<div class="name_box">
+					<span class="name">错错同学</span>
+				</div>
+				
+				<div class="information_box">
+					<span class="information">暂无个人资料</span>
+				</div>
+				
+				<div class="add_photo_edit_information">
+					<div class="add_photo_box">
+						<button type="button" class="add_photo">
+							<svg>
+								<path d="M18.094 2H15s-1-2-2-2H7C6 0 5 2 5 
+								2H2C0 2 0 3.967 0 3.967V14c0 2 2.036 2 2.036 
+								2H17c3 0 3-1.983 3-1.983V4c0-2-1.906-2-1.906-2zM10
+								12c-1.933 0-3.5-1.567-3.5-3.5S8.067 5 10 5s3.5 1.567
+								3.5 3.5S11.933 12 10 12zm0 1.5c-2.76 0-5-2.24-5-5s2.24-5 
+								5-5 5 2.24 5 5-2.24 5-5 5zm7.5-8c-.552 0-1-.448-1-1s.448-1
+								1-1 1 .448 1 1-.448 1-1 1z"  fill-rule="evenodd">
+								</path>
+							</svg>
+							<div class="add_photo_words">上传封面照片</div>
+						</button>
+					</div>
+					
+					
+					<div class="edit_information_box">
+						<button type="button" class="edit_information">
+							编辑个人资料
+						</button>
+					</div>
+				</div>
+			</div >
+		</div>
+	</div>
+	
+	
+	
+	
+	
+	<div class="under">
+		<div class="under_left">
+			<div class="under_navigation">
+				<div class="navigation_box">
+					<a class="navigation">动态<a class="navigation_number">13</a></a>
+					<a class="navigation">回答<a class="navigation_number">13</a></a>
+					<a class="navigation">分享<a class="navigation_number">13</a></a>
+					<a class="navigation">提问<a class="navigation_number">13</a></a>
+					<a class="navigation">收藏<a class="navigation_number">13</a></a>
+					<a class="navigation">关注<a class="navigation_number">13</a></a>				
+				</div>		
+			</div>
+			
+			<div class="my_dynamic_box">
+				<div class="my_dynamic_title_box">
+					<span class="my_dynamic_title">我的动态</span>
+				</div>
+				
+				
+				<div class="questions_have_focus_on_title_box">
+					<span class="questions_have_focus_on_title">关注了问题</span>
+				</div>	
+				
+				<div class="content_of_question_have_focus_on_box">
+					<div class="content_of_question_have_focus_on_box_every">
+						<div class="content_of_question_have_focus_on">
+							学习写代码是种什么体验?
+						</div>
+						<div class="add_focus_on_time">
+							3天前
+						</div>
+					</div>
+				</div>
+				
+				
+			</div>
+		</div>
+		
+		<div class="under_right">
+			<div class="personal_achievement_box">
+				<div class="personal_achievement_title_box">
+					<span class="personal_achievement_title">个人成就</span>
+				</div>
+				<div class="personal_achievement_content_box">
+					<span class="personal_achievement_content">参与两次公共编辑</span>
+				</div>
+			</div>
+			
+			<div class="attention">
+				<div class="number_of_have_pay_attention_to_box">
+					<div class="number_of_have_pay_attention_to_title">关注了</div>
+					<div class="number_of_have_pay_attention_to">0</div>
+				</div>
+				<!--<div class="cutting_line"></div>-->
+				<div class="number_of_pay_attention_to_me_box">
+					<div class="number_of_pay_attention_to_me_title">关注者</div>
+					<div class="number_of_pay_attention_to_me">0</div>	
+				</div>
+			</div>
+			
+			<div class="about_focus">
+				<div class="focus_topic_box">
+					<div class="focus_top_title">关注的话题</div>
+					<div class="focus_top_number">0</div>
+				</div>
+				
+				<div class="focus_column_box">
+					<div class="focus_column_title">关注的专栏</div>
+					<div class="focus_column_number">0</div>
+				</div>
+				
+				<div class="focus_question_box">
+					<div class="focus_question_title">关注的问题</div>
+					<div class="focus_question_number">0</div>
+				</div>
+				
+				<div class="focus_favorites_box">
+					<div class="focus_favorites_title">关注的收藏夹</div>
+					<div class="focus_favorites_number">0</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+		
+
+
+
+
+
+
+
+
+
+</body>
